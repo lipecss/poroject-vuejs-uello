e4d7692cb97db497949ae4d7ae79892917204934
Felipecss <felipecss@vemlavaralouca.com.br>
2021-02-28 18:19:48 -0300

    Pagina simples de login configurada

--- src/views/website/LoginPage.vue
+++ src/views/website/LoginPage.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
-    <h1>Login Page View</h1>
-    <p>{{ $t('pages.website.loginPage.title') }}</p>
+  <b-container fluid class="login-page">
+    <b-row>
+      <b-col cols="12" xs="12" class="login-page__content" order="2">
+        <div class="login-page__content--logo" order="2">
+          <a href="https://www.uello.com.br/" target="_blank">
+            <img src="https://www.uello.com.br/dist/images/logo.png" alt="Uello Logo" />
+          </a>
+        </div>
 
-    <input v-model="loginData.username" type="text" :placeholder="$t('inputs.username.placeholder')" :disabled="blockUi"><br>
-    <input v-model="loginData.password" type="password" :placeholder="$t('inputs.password.placeholder')" :disabled="blockUi"><br><br>
+        <div class="login-page__content--form" order="1">
+          <h1>Olá Parceiro</h1>
+          <p>{{ $t('pages.website.loginPage.title') }}</p>
 
-    <button @click="loginUser($event)" :disabled="blockUi">{{ $t('buttons.signIn') }}</button>
-  </div>
+          <b-form-input
+          v-model="loginData.username"
+          size="md"
+          type="text"
+          :placeholder="$t('inputs.username.placeholder')"
+          :disabled="blockUi"
+          /><br>
+
+          <b-form-input
+          v-model="loginData.password"
+          size="md"
+          type="password"
+          :placeholder="$t('inputs.password.placeholder')"
+          :disabled="blockUi"
+          /><br>
+
+          <button
+          @click="loginUser($event)"
+          :disabled="blockUi"
+          class="login-page__content--form--button">
+            {{ $t('buttons.signIn') }}
+          </button>
+        </div>
+
+        <div class="login-page__content--footer">
+          <p>Copyright © 2021 Uello. All Rights Reserved.</p>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -123,5 +157,113 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+.login-page {
+  .login-page__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: $primary;
+    background: linear-gradient(-45deg, $monotone-primary, $pink-channel, $monotone-primary);
+    background-size: 400% 400%;
+    animation: gradientBG 15s ease-in infinite;
+
+    &--logo {
+      position: absolute;
+      top: 90px;
+      width: 150px;
+      height: 150px;
+      max-width: 150px;
+      transform: translatey(0px);
+      animation: float 3s ease-in-out infinite;
+      z-index: 10;
+
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+
+    &--form {
+      position: relative;
+      width: 362px;
+      margin: 38px;
+      top: 80px;
+      text-align: center;
+      h1 {
+        font-weight: 700;
+        text-shadow: 0px 1px #fff;
+      }
+
+      &--button {
+        bottom: 15px;
+        right: 20px;
+
+        background: transparent;
+        color: $gray;
+        border: 1px solid $gray;
+        padding: .375rem .75rem;
+        border-radius: 20px;
+        text-transform: uppercase;
+        font-weight: 500;
+        transition: 0.5s;
+
+        &:hover {
+          background: $gray;
+          color: $hover-secondary;
+          transform: translateY(-5px);
+        }
+      }
+    }
+    &--footer {
+      position: absolute;
+      bottom: 0;
+      font-weight: 700;
+    }
+  }
+
+  @media screen and (min-width: 375px) {}
+  @media screen and (min-width: 425px) {}
+  @media screen and (min-width: 576px) {}
+  @media screen and (min-width: 768px) {}
+  @media screen and (min-width: 1024px) {
+    .login-page__content {
+      &--form {
+        top: 0px;
+      }
+    }
+  }
+  @media screen and (min-width: 1400px) {}
+  @media screen and (min-width: 1600px) {}
+}
+
+.row {
+  height: 100vh;
+}
+
+// Animations
+@keyframes gradientBG {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes float {
+  0% {
+    transform: translatey(0px);
+  }
+  50% {
+    transform: translatey(-20px);
+  }
+  100% {
+    transform: translatey(0px);
+  }
+}
+
 </style>
